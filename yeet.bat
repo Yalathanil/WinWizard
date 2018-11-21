60258b264415a6be60c636b7535f36a1b39ff432
Yalathanil <45241949+Yalathanil@users.noreply.github.com>
2018-11-21 10:39:36 -0800

    Create yeet.bat

---
+++ yeet.bat
@@ -0,0 +1,77 @@
+@echo off
+title virus.exe
+call :Yeet
+cmd /k
+goto :eof
+
+
+:Yeet
+color 
+echo _______________________________________________________
+echo *******************************************************
+echo _______________________________________________________
+echo -
+echo - Attention windows user!
+echo - 
+timeout 2 > null
+echo - System 32 needs your help!
+echo -
+timeout 2 > null
+echo - Virus.exe has infected your system
+echo -
+timeout 2 > null 
+echo - You must not allow Virus.exe to kill our friend system 32!
+echo -
+timeout 2 > null
+echo - All you need is to enter your moms credit card number, the expiration date and three wack numbers on the back!
+echo -
+echo _______________________________________________________
+set /p cardnum="Enter Card Number: " 
+echo _______________________________________________________
+set /p expdate="Enter Expiration Date (mm/dd): "
+echo _______________________________________________________
+set /p seccode="Enter Security Code: "
+timeout 2 > null
+echo - Alert! Your security code did not match the card number!
+echo -
+timeout 2 > null
+echo - Your payment could not be processed!
+echo -
+timeout 2 > null
+echo - Virus.exe has surpassed power level 5000!
+echo -
+timeout 2 > null
+echo - Naruto.exe can no longer defend your computer!
+echo - 
+timeout 2 > null
+echo - Native tribes have resorted to canibalism!
+echo - 
+timeout 2 > null
+echo - The files are already Tracer!
+echo - 
+timeout 2 > null
+echo - Initializing Thanossnap.exe
+timeout 1 > null
+echo 5
+timeout 1 > null
+echo 4 
+timeout 1 > null
+echo 3
+timeout 1 > null
+echo 2
+timeout 1 > null
+echo 1 
+timeout 1 > null
+color 4
+echo *SNAP*
+timeout 5 > null
+tasklist
+cd /windows
+tree
+echo Virus detected, you will be rediriected shortly
+timeout 2 > null
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+Color A
+
+
+exit /b 0
