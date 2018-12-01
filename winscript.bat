243a0fac7c2898bc0198b5e6b2d2d7689e0a127f
Yalathanil <45241949+Yalathanil@users.noreply.github.com>
2018-12-01 11:14:54 -0800

    Update winscript.bat

--- winscript.bat
+++ winscript.bat
@@ -6,7 +6,16 @@ echo  Welcome to the glorious windows script
 echo ----------------------------------------
 echo.
 echo.
-goto :M
+
+
+Echo Initializing
+::Choco lets us download a variety of things that are important. 
+::Once set up it can be called by choco install blank
+::Need to add a templates that can be downloaded and put in for security settings and the like
+::Along with a checklist
+PowerShell Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+pause
+
 
 :M
 Echo ----------------------------------------
@@ -22,17 +31,19 @@ echo Firewall -- 3
 echo Media    -- 4
 echo Users    -- 5
 echo Policies -- 6
+echo   -- 7
+echo Update   -- 8
 echo Auto     -- A
 echo Menu     -- M
 echo.
-Choice /C 123456AM /N /M "Please choose a functionality: "
+Choice /C 12345678AM /N /M "Please choose a functionality: "
 goto %errorlevel%
 
 
 ::Disables Vulnerable Services
 :1
 Echo Disabling Vulnerable Services
-sc config SessionEnv start= disabled 
+sc config SessionEnv start=  disabled 
 sc stop SessionEnv 
 sc config IISADMIN start= disabled 
 sc stop IISADMIN
@@ -46,13 +57,22 @@ sc config RemoteAccess start= disabled
 sc stop RemoteAccess
 
 echo Enabling Important Services
-sc config MpsSvc start= enabled
+sc config MpsSvc start= auto
+sc start MpsSvc 
 goto :M
 
 ::Turns on all Auditing
 :2
-Echo Auditing Enabled
-auditpol /set /category:* /success:enable /failure:enable
+Echo Enabling Auditing
+auditpol /set /category:"logon/logoff" /success:enable /failure:enable
+auditpol /set /category:"Account Logon" /success:enable /failure:enable
+auditpol /set /category:"Account Management" /success:enable /failure:enable
+auditpol /set /category:"Detailed Tracking" /success:enable /failure:enable
+auditpol /set /category:"DS Access" /success:enable /failure:enable
+auditpol /set /category:"Object Access" /success:enable /failure:enable
+auditpol /set /category:"Policy Change" /success:enable /failure:enable
+auditpol /set /category:"Privilege Use" /success:enable /failure:enable
+auditpol /set /category:"System" /success:enable /failure:enable
 goto :M
 
 ::Turns on Firewall
@@ -63,6 +83,7 @@ goto :M
 
 ::Serches for Media Files
 :4
+:beginMedia
 Echo Media Management Menu
 echo.
 echo Purge system of all media -- 1
@@ -70,19 +91,48 @@ echo Generate log of all media -- 2
 echo Search for particular ext -- 3
 echo Return to menu            -- M
 set /p mediaf="Enter Selection Here: "
-if [mediaf]==[1] (
+if [%mediaf%]==[1] (
 	del /p  %homedrive%\*.mp3 /s /a-s
+	del /p  %homedrive%\*.mp4 /s /a-s
+	del /p  %homedrive%\*.png /s /a-s
+	del /p  %homedrive%\*.jpg /s /a-s
+	del /p  %homedrive%\*.mov /s /a-s
+	del /p  %homedrive%\*.avi /s /a-s
+	del /p  %homedrive%\*.wmv /s /a-s
+	goto :beginMedia
 )
-Type NUL > MediaLog.txt
-cd %homedrive%\
-dir /S /B /A *.txt *.mp3 *.mp4 *.mov *.wav >> MediaLog.txt
-start MediaLog.txt
-pause
-goto :M
+if [%mediaf%]==[2] (
+	Type NUL > MediaLog.txt
+	cd %homedrive%\
+	dir /S /B /A *.txt *.mp3 *.mp4 *.mov *.wav *.bat *.vbi >> MediaLog.txt
+	start MediaLog.txt
+	echo MediaLog.txt will be deleted once done
+	pause
+	del MediaLog.txt
+	goto :beginMedia
+)
+if [%mediaf%]==[3] (
+	Type NUL > MediaLog.txt
+	set /p mediaext="Enter extention to search for (.): "
+	cd %homedrive%\
+	dir /S /B /A *.%mediaext% >> MediaLog.txt
+	start MediaLog.txt
+	echo MediaLog.txt will be deleted once done
+	pause
+	del MediaLog.txt
+	goto :beginMedia
+)
+if [%mediaf%]==[M] (
+	goto :M
+)
+echo Incorrect value entered.
+goto :beginMedia
+
 
 ::User/Group Management
 :5
 setlocal
+net user guest /active:no
 ::Users
 :beginUser
 net users
@@ -148,22 +198,32 @@ if [%uservar%]==[3] (
 		)
 		
 )
-
-if [%uservar%]==[M] (
-	goto :M
+		if [%uservar%]==[M] (
+			goto :M
 )
-
-else (
-	echo Incorrect value entered
-	goto :beginning
-)
-net user guest /active:no
+echo Incorrect value entered
+goto :beginning
 endlocal
 goto :beginUser
 
 ::Policies
 :6
 net accounts /minpwlen:8 /maxpwage:90 /minpwage:15 /uniquepw:24
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers /v DisableAutoplay /t REG_DWORD /d 0 
+secedit /import /db secedit.sbd /cfg c:\Users\%userprofile%\Desktop\Win10SecPolTemp.inf
 goto :M
+
+::Updates
+:8
+echo Initializing 3rd Pary Program Downloads and Updates
+choco update Firefox
+choco install sysinternals
+choco install ie11
+choco install malwarebytes
+echo Initializing Windows Updates
+pause
+wuauclt /detectnow /updatenow
+goto :M
+
 
 
