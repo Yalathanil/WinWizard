07c2da9d9660b33e1ca45b1fbdb77e8b9fb4dd3f
Yalathanil <45241949+Yalathanil@users.noreply.github.com>
2018-12-07 19:42:13 -0800

    Update winscript.bat

--- winscript.bat
+++ winscript.bat
@@ -16,7 +16,7 @@ Echo Initializing
 echo.
 echo If you want to skip the initial step, enter M
 set /p Init="Enter here: "
-if Init==M (
+if %Init%==M (
 	goto :M
 	)
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
